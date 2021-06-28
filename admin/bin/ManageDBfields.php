560356667ff390658033226ff3efd6725b4e0b38
Kevin Mungai <vokenp@gmail.com>
2021-06-28 12:57:45 +0300

    Db Fiels Changes

--- admin/bin/ManageDBfields.php
+++ admin/bin/ManageDBfields.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("../../timeout.php");
  include("../../assets/bin/con_db.php");
  global $db;
@@ -8,36 +8,38 @@ ini_set('display_errors', 1);
  $user = $_SESSION['user'];
  if (!isset($_SESSION['user'])) {
      header("Location: ../index.php");
-     die();     
+     die();
 }
 
 $dict = NewDataDictionary($db);
 
- 
- if (isset($_POST['CreateNewTbl'])) 
+ if (isset($_POST['CreateNewTbl']))
  {
  	$TableName = safehtml($_POST['CreateNewTbl']);
  	$columns = "S_ROWID R AUTO PRIMARY KEY,CreatedBy C(255) DEFAULT 'ADMIN',DateCreated T DEFTIMESTAMP,ModifiedBy C(255),DateModified T";
 	$sqlarray = $dict->createTableSql($TableName,$columns);
 	$exec = $dict->executeSqlArray($sqlarray);
 	 if ($exec) {
+     $execu = $db->Execute("ALTER TABLE $TableName MODIFY DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,MODIFY DateModified TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+     $execu = $db->Execute("insert into $TableName(CreatedBy) values('Admin')");
+     $execu = $db->Execute("delete from $TableName");
 	 	echo "Success";
 	 }
  }
 
  if (isset($_POST['AlterColumn'])) {
- 	
+
  }
 
 
  if (isset($_POST['btnAddColumn'])) {
- 	 
+
  	$CurrentTable = safehtml($_POST['CurrentTable']);
     $ColumnName = safehtml($_POST['ColumnName']);
     $DisplayName = safehtml($_POST['DisplayName']);
     $DataType = safehtml($_POST['DataType']);
     $DefaultValue = safehtml($_POST['DefaultValue']);
-    
+
     $Ctypes = array("C","C2");
     $ColLen = in_array($DataType, $Ctypes) ? "(255)" : "";
     $DefValue = $DefaultValue != "" ? "NOTNULL DEFAULT '$DefaultValue'" : "NULL";
@@ -48,10 +50,10 @@ $dict = NewDataDictionary($db);
       {
       	$columns = "$ColumnName $DataType".$ColLen." $DefValue";
       }
-    
+
     $sqlArray = $dict->addColumnSql($CurrentTable,$columns);
 
-        $dict->executeSqlArray($sqlArray);	
+        $dict->executeSqlArray($sqlArray);
    if ($dict) {
 	 	echo "Success";
 
@@ -79,7 +81,7 @@ $dict = NewDataDictionary($db);
 	 {
 	 	echo "Error :".$dict->ErrorMsg();
 	 }
- 
+
  }
 
 
@@ -87,7 +89,7 @@ $dict = NewDataDictionary($db);
     $TableName = safehtml($_POST['RenameColumn']);
  	$OldColName = safehtml($_POST['OldColName']);
  	$NewColName = safehtml($_POST['NewColName']);
- 	
+
  	  if (DB_DRIVER == "mysqli") {
  	  	 $ar = $db->metaColumns($TableName);
          $cols = array();
@@ -95,21 +97,21 @@ $dict = NewDataDictionary($db);
   	      $cols[$key] = (array)$value;
          }
          $ColDef = $cols[strtoupper($OldColName)];
-        
+
           $Ctypes = array("C","C2");
          $AdoType = $db->metaType($ColDef["type"]);
          $MaxLen = $ColDef["max_length"];
          $ColLen = in_array($AdoType, $Ctypes) ? "($MaxLen)" : "";
          $flds = "$NewColName $AdoType".$ColLen;
-          
+
  	  	$sqlArray  = $dict->renameColumnSql($TableName,$OldColName,$NewColName,$flds);
  	  }
  	  else
  	  {
  	  	$sqlArray  = $dict->renameColumnSql($TableName,$OldColName,$NewColName);
  	  }
-    
-   $exec =  $dict->executeSqlArray($sqlArray);	
+
+   $exec =  $dict->executeSqlArray($sqlArray);
 
    if ($exec) {
 	 	echo "Success";
@@ -123,12 +125,12 @@ $dict = NewDataDictionary($db);
     $sqlArray  = $dict->dropColumnSql($TableName,$ColName);
     $dict->executeSqlArray($sqlArray);
 
-    $exec =  $dict->executeSqlArray($sqlArray);	
+    $exec =  $dict->executeSqlArray($sqlArray);
 
    if ($exec) {
 	 	echo "Success";
 	 }
- 
+
  }
 
 ?>
