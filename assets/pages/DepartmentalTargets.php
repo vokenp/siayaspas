e98023ad92212e4269e2815fe0774f83ccd57605
vokenp <vokenp@gmail.com>
2021-09-14 15:04:53 +0300

    dd

--- assets/pages/DepartmentalTargets.php
+++ assets/pages/DepartmentalTargets.php
@@ -95,7 +95,7 @@
     <div id="colAlert"></div>
         <input type="hidden" name="ReturnType" id="ReturnType2" value="RstID">
      <input type="hidden" name="S_ROWID" id="S_ROWID2" >
-
+   <div class="row">
      <div class="form-group">
          <label class="col-sm-3 control-label " for="PhoneNo"> Objective</label>
           <div class="col-sm-9">
@@ -103,6 +103,24 @@
          </div>
        </div>
 
+       <div class="form-group col-sm-10">
+        <label class="col-sm-4 control-label " for="UserID" >Target AssignedTo</label>
+        <div class="col-sm-8">
+          <select id="AssignedTo2" name="AssignedTo"    class="col-xs-12 col-sm-12 chosen-select">
+            <?php
+                $getUsers = $db->GetArray("select *from vw_userslist where   order by S_ROWID desc");
+                 echo "<option value=''></option>";
+                foreach ($getUsers as $ukey => $uval) {
+                  $UserID = $uval["loginid"];
+                  $FullName = $uval["Fullname"];
+                  echo "<option value='$UserID'>$FullName</option>";
+                }
+            ?>
+          </select>
+        </div>
+      </div>
+
+   </div>
       </div><!-- End ModalBody -->
 <div class="modal-footer text-center">
   <button type="submit" id="btnUpdateRecord" name="btnUpdateRecord"  class="btn btn-sm btn-success">
