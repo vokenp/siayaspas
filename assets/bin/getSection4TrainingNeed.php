3e74492633d03b61b6847887617e2e4d9f26c995
vokenp <vokenp@gmail.com>
2021-09-17 11:58:15 +0300

    ss

---
+++ assets/bin/getSection4TrainingNeed.php
@@ -0,0 +1,56 @@
+<?php
+include("../timeout.php");
+ include("con_db.php");
+global $db;
+//$db->debug=1;
+
+      $CurTable = "tbl_section4";
+      $search = $_POST['search'];
+      $AppraisalID = $_POST['AppraisalID'];
+
+   $recdata = array();
+   $cols = $db->metaColumnNames($CurTable,true);
+   $k = 0;
+
+   $Order = $_POST['order'];
+  $OrderColumn = $cols[$Order[0]["column"]];
+  $OrderDire = $Order[0]["dir"];
+
+   $where = "where AppraisalID='$AppraisalID'  ";
+
+   $getData = $db->SelectLimit("select *from $CurTable $where order by S_ROWID asc",$_POST['length'],$_POST['start']);
+   $count = $getData->RecordCount();
+    while (!$getData->EOF) {
+      $rst = array();
+
+        $rst = $getData->fields;
+
+      $S_ROWID = $rst["S_ROWID"];
+      $rowData = json_encode($rst);
+
+     $ResetPswd = "<a href='#' onclick='doRemoveTrainingNeed(\"$S_ROWID\"); return false;' title='Remove Training'><span class='red'><i class='ace-icon fa fa-trash bigger-120'></i></span></a>";
+     $EditColumn = "<a id='row-$S_ROWID' href='#' onclick='DoEditTrainingNeed(\"$S_ROWID\");  return false;' title='Edit Training' data-value='$rowData'><span class='yellow'><i class='ace-icon fa fa-pencil-square-o bigger-120'></i></span></a>";
+
+      $ActionList =  $EditColumn.str_repeat("&nbsp;", 5).$ResetPswd;
+
+
+     // $ActionList = $AppCode == "General" ? "" : $ActionList;
+      $k +=1;
+      $record = array();
+      $record[] = $k;
+      $record[] = $rst["TrainingNeed"];
+      $record[] = $rst["TrainingPeriodNo"]." ".$rst["TrainingPeriodUnit"];
+      $record[] = $rst["SA_Comments"];
+      $record[] = $ActionList;
+      $recdata[] = $record;
+
+    $getData->MoveNext();
+    }
+
+   $array = array();
+   $array["draw"] = $_POST['draw'];
+   $array["recordsTotal"] = $count;
+   $array["recordsFiltered"] = $count;
+   $array["data"] =  $recdata;
+   echo json_encode($array);
+?>
--- assets/pages/AppSection4.php
+++ assets/pages/AppSection4.php
@@ -1,6 +1,334 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		var op = $("#op").val();
+    dotoken();
+
+
+    //Create New tblTrainingNeed
+    $("#frmNewTrainingNeed").validate({
+  debug: false,
+  rules: {
+
+  },
+  messages: {
+
+  },
+  submitHandler: function(form) {
+  // do other stuff for a valid form
+
+     $.post('assets/bin/ManageRecords.php', $("#frmNewTrainingNeed").serialize(), function(data) {
+
+      if (data.length < 30)
+      {
+      $(".close").click();
+      var frm = "#frmNewTrainingNeed";
+      $(frm)[0].reset();
+      $(frm).trigger("reset");
+      $(frm).find(":submit").prop('disabled', false);
+      $(frm).find(":submit").html("<i class='fa fa-plus'></i> Create Activity");
+      $(frm).data('submitted', false);
+      $(frm).modal("hide");
+       dotoken();
+      $('#tblTrainingNeed').DataTable().draw();
+          Swal.fire({
+                  type: 'success',
+                  title: 'Training Need Save Successful',
+                  showConfirmButton: false,
+                  timer: 1500
+                  });
+      }
+      else
+      {
+      dotoken();
+      Swal.fire(
+                    'Oops!',
+                    data,
+                    'error'
+                  );
+
+      }
+  });
+  }
+  });
+
+
+  // Update Objectives
+  $("#frmUpdateTrainingNeed").validate({
+debug: false,
+rules: {
+
+},
+messages: {
+
+},
+submitHandler: function(form) {
+// do other stuff for a valid form
+
+ $.post('assets/bin/ManageRecords.php', $("#frmUpdateTrainingNeed").serialize(), function(data) {
+
+  if (data.length < 30)
+  {
+  $(".close").click();
+  var frm = "#frmUpdateTrainingNeed";
+  $(frm)[0].reset();
+  $(frm).trigger("reset");
+  $(frm).find(":submit").prop('disabled', false);
+  $(frm).find(":submit").html("<i class='fa fa-edit'></i> Update Activity");
+  $(frm).data('submitted', false);
+  $(frm).modal("hide");
+   dotoken();
+  $('#tblTrainingNeed').DataTable().draw();
+      Swal.fire({
+              type: 'success',
+              title: 'Training Need Update Successful',
+              showConfirmButton: false,
+              timer: 1500
+              });
+  }
+  else
+  {
+  dotoken();
+  Swal.fire(
+                'Oops!',
+                data,
+                'error'
+              );
+
+  }
+});
+}
+});
+
+    //Start TABLE
+    var dataTableActivites = $('#tblTrainingNeed').DataTable({
+       "Processing": true,
+       "serverSide": true,
+       "scrollY": "50vh",
+       "scrollCollapse": true,
+       "bFilter":false,
+       "ordering": true,
+       "bLengthChange": true,
+       "bPaginate": true,
+       "pagingType": "simple",
+       language: {
+      paginate: {
+      next: '<i class="fa fa-chevron-right">',
+      previous: '<i class="fa fa-chevron-left">'
+     }
+     },
+
+       "ajax":{
+          url :"assets/bin/getSection4TrainingNeed.php", // json datasource
+          type: "post",  // type of method  , by default would be get
+         "data":function(data) {
+          data.AppraisalID   = $('#S_ROWID').val();
+
+          },
+
+          error: function(et){  // error handling code
+            $("#tblTrainingNeed_processing").css("display","none");
+            alert(JSON.stringify(et));
+          }
+        },
+
+  });
+    //End Table
+
+
+  });
+
+
+  function DoEditTrainingNeed(RowID)
+       {
+        var RowInfo = eval('(' + $("#row-"+RowID).attr('data-value') + ')');
+        $("#TrainingNeed2").html(RowInfo.TrainingNeed);
+        $("#TrainingPeriodNo2").val(RowInfo.TrainingPeriodNo);
+        $("#SA_Comments2").val(RowInfo.SA_Comments);
+        $("#S_ROWID_Training").val(RowInfo.S_ROWID);
+       //	$("#ItemType2").trigger("chosen:updated");
+        $("#UpdateTrainingNeed").modal("show");
+       }
+
+  function doRemoveTrainingNeed(RowID)
+       {
+            bootbox.confirm({
+                centerVertical: true,
+            message: "Are you sure you want remove this Training Need?",
+            buttons: {
+              confirm: {
+               label: "Remove Item",
+               className: "btn-danger btn-sm",
+              },
+              cancel: {
+               label: "Cancel",
+               className: "btn-sm",
+              }
+            },
+            callback: function(result) {
+              if(result)
+              {
+
+
+                 var dialog = bootbox.dialog({
+                  title: "Remove Objective",
+                  centerVertical: true,
+             message: '<p class="text-center mb-0"><i class="fa fa-spin fa-cog bigger-240"></i> <h3>Please wait ...</h3></p>',
+             closeButton: false
+               });
+    var postdata = $("#myForm").serializeArray();
+    postdata.push({name: '_token', value: $("#_token").val()});
+     postdata.push({name: 'ModCode', value: "70"});
+    postdata.push({name: 'DeleteRecord', value: RowID});
+    $.post("assets/bin/ManageRecords.php", postdata, function(data){
+
+         dialog.modal('hide');
+         $('#tblTrainingNeed').DataTable().draw();
+         dotoken();
+           Swal.fire({
+                type: 'success',
+                title: 'Training Need Removed Successful',
+                showConfirmButton: false,
+                timer: 1500
+                });
+    });
+
+              }
+            }
+            });
+       }
+
+  </script>
 <div>
+
   <h3 class="header smaller lighter blue">Section 4 : STAFF TRAINING AND DEVELOPMENT PLAN</h3>
   <div class="alert alert-info">
-			This Section should be completed by the Appraisee in consultation with the Supervisor
+			To be completed by the Appraisee as Agreed with the Supervisor
 	</div>
 </div>
+<div class="row">
+  <div class="col-xs-12">
+        <div class="widget-box">
+          <div class="widget-header widget-header-flat">
+            <h4 class="widget-title smaller">
+              <i class="ace-icon fa fa-list smaller-80"></i>
+                Training and Development Needs
+            </h4>
+            <div class="widget-toolbar no-border">
+              <a data-toggle='modal' href='#CreateNewTrainingNeed' class='dt-button btn  btn-success btn-bold' title='Add New'><i class='fa fa-plus  fa-lg'></i> Create New Training Need </a>
+             </div>
+          </div>
+        <div class="widget-body">
+          <table id="tblTrainingNeed" class="table table-bordered table-striped">
+              <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Training Need</th>
+                    <th>Training Duration</th>
+                    <th>Appraisee Comment</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+            </table>
+
+        </div>
+    </div><!-- WidgetBox -->
+  </div><!-- RightSide -->
+</div>
+
+
+<div id="CreateNewTrainingNeed" class="modal fade" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3 id="Colh3" class="smaller lighter blue no-margin"><i class="ace-icon fa fa-user icon-on-right bigger-110"></i> Create New Training Need </h3>
+      </div>
+ <form name="frmNewTrainingNeed" id="frmNewTrainingNeed" class="form-horizontal" role="form">
+  <div class="modal-body">
+  <input type="hidden" name="ModCode" id="ModCode" value="70">
+  <input type="hidden" name="AppraisalID" id="AppraisalID" value="<?php echo $rst["S_ROWID"];?>">
+
+   <input type="hidden" name="_token" id="_token2" class="token" >
+    <div id="colAlert"></div>
+        <input type="hidden" name="ReturnType" id="ReturnType2" value="RstID">
+   <div class="row">
+    <div class="form-group col-sm-12">
+        <label class="col-sm-4 control-label " for="TrainingNeed"> Training Need</label>
+         <div class="col-sm-8">
+           <textarea name="TrainingNeed" id="TrainingNeed" class="form-control" rows="3"></textarea>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+     <div class="form-group col-sm-12">
+         <label class="col-sm-4 control-label " for="SA_Comments"> Appraisee Comments</label>
+          <div class="col-sm-8">
+            <textarea name="SA_Comments" id="SA_Comments" class="form-control" rows="3"></textarea>
+         </div>
+       </div>
+     </div>
+
+
+
+
+
+
+      </div><!-- End ModalBody -->
+<div class="modal-footer">
+  <button type="submit" id="btnSaveRecord" name="btnSaveRecord"  class="btn btn-sm btn-success">
+         <i class="ace-icon fa fa-plus icon-on-right bigger-110"></i> Create Training Need
+      </button>
+        </div>
+      </form>
+    </div><!-- Modal-content -->
+  </div><!-- Modal-Dialog -->
+ </div><!-- Modal-Div -->
+
+
+
+ <div id="UpdateTrainingNeed" class="modal fade" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3 id="Colh3" class="smaller lighter blue no-margin"><i class="ace-icon fa fa-user icon-on-right bigger-110"></i> Update Training Need</h3>
+      </div>
+ <form name="frmUpdateTrainingNeed" id="frmUpdateTrainingNeed" class="form-horizontal" role="form">
+  <div class="modal-body">
+   <input type="hidden" name="ModCode" id="ModCode" value="70">
+   <input type="hidden" name="_token" id="_token2" class="token" >
+    <div id="colAlert"></div>
+        <input type="hidden" name="ReturnType" id="ReturnType2" value="RstID">
+     <input type="hidden" name="S_ROWID" id="S_ROWID_Training" >
+
+     <div class="row">
+      <div class="form-group col-sm-12">
+          <label class="col-sm-4 control-label " for="TrainingNeed"> Training Need</label>
+           <div class="col-sm-8">
+             <textarea name="TrainingNeed" id="TrainingNeed2" class="form-control" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+       <div class="form-group col-sm-12">
+           <label class="col-sm-4 control-label " for="SA_Comments"> Appraisee Comments</label>
+            <div class="col-sm-8">
+              <textarea name="SA_Comments" id="SA_Comments2" class="form-control" rows="3"></textarea>
+           </div>
+         </div>
+       </div>
+
+      </div><!-- End ModalBody -->
+<div class="modal-footer text-center">
+  <button type="submit" id="btnUpdateRecord" name="btnUpdateRecord"  class="btn btn-sm btn-success">
+        Update Training Need
+        <i class="ace-icon fa fa-edit icon-on-right bigger-110"></i>
+      </button>
+        </div>
+      </form>
+    </div><!-- Modal-content -->
+  </div><!-- Modal-Dialog -->
+ </div><!-- Modal-Div -->
