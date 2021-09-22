f785655ae068eaf874a9839d30cea919a4a7067d
vokenp <vokenp@gmail.com>
2021-09-22 19:38:29 +0300

    aa

--- assets/pages/AppSection3.php
+++ assets/pages/AppSection3.php
@@ -81,9 +81,7 @@
 <div>
 
   <h3 class="header smaller lighter blue">Section 3 : PERFORMANCE TARGETS</h3>
-  <div class="alert alert-info">
-			This Section should be completed by the Appraisee in consultation with the Supervisor
-	</div>
+  
 </div>
 <div class="row">
   <div id="ValueOptions" class="col-xs-12">
