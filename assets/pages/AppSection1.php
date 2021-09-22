e0e2f5404211bed53a185d2b153fab1a16cc6085
vokenp <vokenp@gmail.com>
2021-09-22 19:47:13 +0300

    S1

--- assets/pages/AppSection1.php
+++ assets/pages/AppSection1.php
@@ -1,49 +1,7 @@
 <script type="text/javascript">
 
 	$(document).ready(function(){
-    $("#frmChangeSuper").validate({
-  debug: false,
-  rules: {
 
-  },
-  messages: {
-
-  },
-  submitHandler: function(form) {
-  // do other stuff for a valid form
-  $.post('assets/bin/ManageRecords.php', $("#frmChangeSuper").serialize(), function(data) {
-
-    if (data.length < 15)
-{
-$(".close").click();
-var frm = "#frmChangeSuper";
-$(frm)[0].reset();
-$(frm).trigger("reset");
-$(frm).find(":submit").prop('disabled', false);
-$(frm).find(":submit").html("<i class='fa fa-send'></i> Change Supervisor");
-$(frm).data('submitted', false);
-$(frm).modal("hide");
-Swal.fire({
-        type: 'success',
-        title: 'Save Successful',
-        showConfirmButton: false,
-        timer: 1500
-        });
-        location.reload();
-}
-else
-{
-  Swal.fire(
-      'Oops!',
-      data,
-      'error'
-    );
-location.reload();
-
-}
-  });
-  }
-  });
 
   });
   </script>
@@ -122,7 +80,7 @@ location.reload();
   <div class="profile-info-name"> Supervisor</div>
   <div class="profile-info-value">
     <span class="editable" id="username"><b><?php  echo $rst["SupervisorName"];?></b>
-    <a href="#ChangeSupv" data-toggle="modal" class="pull-right" title="Change Supervisor"> <i class="ace-icon fa fa-edit blue fa-md"></i> Change Supervisor</a></span>
+
   </div>
 </div>
 
@@ -137,53 +95,3 @@ location.reload();
 </div>  <!-- End Col -->
 </div> <!-- End Row-->
 </div>
-
-
-<div id="ChangeSupv" class="modal fade" >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h3 id="Colh3" class="smaller lighter blue no-margin">Change Supervisor </h3>
-        </div>
-        <form name="frmChangeSuper" id="frmChangeSuper" class="form-horizontal" role="form">
-        <div class="modal-body">
-
-          <div id="colAlert"></div>
-          <input type="hidden" name="_token" id="_token" value="<?php echo $token; ?>" class="token">
-          <input type="hidden" name="ModCode" id="ModCode" value="<?php echo $mod;?>">
-          <input type="hidden" name="S_ROWID" id="S_ROWID" value="<?php echo $cid;?>">
-          <input type="hidden" name="ReturnType" id="ReturnType" value="RstID">
-
-
-   <div class="row">
-     <div class="form-group col-sm-10">
-      <label class="col-sm-4 control-label " for="UserID" >Select Supervisor </label>
-      <div class="col-sm-8">
-        <select id="SupervisorUserID" name="SupervisorUserID" required="true"   class="col-xs-12 col-sm-12 chosen-select">
-          <?php
-          $curSupervisor = $rst["SupervisorUserID"];
-              $getUsers = $db->GetArray("select *from vw_userslist where loginid<>'$user'  and loginid<>'$curSupervisor' order by S_ROWID desc");
-               echo "<option value=''></option>";
-              foreach ($getUsers as $ukey => $uval) {
-                $UserID = $uval["loginid"];
-                $FullName = $uval["Fullname"];
-                echo "<option value='$UserID'>$FullName</option>";
-              }
-          ?>
-        </select>
-      </div>
-    </div>
-  </div>
-
-        </div><!-- End ModalBody -->
-  <div class="modal-footer">
-  <button type="submit" id="btnUpdateRecord" name="btnUpdateRecord"  class="btn btn-sm btn-success">
-          Change Supervisor
-          <i class="ace-icon fa fa-plus icon-on-right bigger-110"></i>
-        </button>
-          </div>
-        </form>
-      </div><!-- Modal-content -->
-    </div><!-- Modal-Dialog -->
-   </div><!-- Modal-Div -->
